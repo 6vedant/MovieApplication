ea62bf14f2a32167fdafc2e121282dce2838c285
Vedant Jha <6vedant@gmail.com>
2024-05-06 16:30:43 +0530

    Inital Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,92 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.vedantjha.browsemovies"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.vedantjha.browsemovies"
+        minSdk = 34
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    // define library versions
+    val appCompatVersion = "1.6.1"
+    val constraintLayoutVersion = "2.1.2"
+    val coroutines = "1.5.2"
+    val lifecycleVersion = "2.4.0"
+    val materialVersion = "1.4.0"
+    val roomVersion = "2.4.0-alpha05"
+    val retrofitVersion = "2.9.0"
+    val hiltVersion = "2.44"
+    // testing
+    val junitVersion = "4.13.2"
+    val espressoVersion = "3.4.0"
+    val kotlin_version = "1.9.0"
+
+    // android core components
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:$appCompatVersion")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:$constraintLayoutVersion")
+
+    // hilt componenets
+    implementation("com.google.dagger:hilt-android:$hiltVersion")
+    ksp("com.google.dagger:hilt-android-compiler:$hiltVersion")
+
+    // Room components
+    implementation("androidx.room:room-ktx:$roomVersion")
+    ksp("androidx.room:room-compiler:$roomVersion")
+    androidTestImplementation("androidx.room:room-testing:$roomVersion")
+
+    // Lifecycle components
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-common-java8:$lifecycleVersion")
+
+    // Kotlin components
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlin_version")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutines")
+
+    // UI
+    implementation("androidx.constraintlayout:constraintlayout:$constraintLayoutVersion")
+    implementation("com.google.android.material:material:$materialVersion")
+
+    //Retrofit Dependecies.
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+
+    testImplementation("junit:junit:$junitVersion")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:$espressoVersion")
+}
---
+++ app/src/main/java/com/vedantjha/browsemovies/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.vedantjha.browsemovies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
